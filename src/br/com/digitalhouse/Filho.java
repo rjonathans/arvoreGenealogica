a52ffdd88c713080862f29f23ee398ae775ac15e
unknown <Digital@DH-A8-22.dhbrasil.digitalhouse.com>
2019-04-05 21:10:36 -0300

    Initial commit

---
+++ src/br/com/digitalhouse/Filho.java
@@ -0,0 +1,35 @@
+package br.com.digitalhouse;
+
+public class Filho {
+
+    private String mae;
+    private String pai;
+    private String nome;
+
+    //Construtor especifico
+    public Filho(String nomeMa)
+
+    public String getMae() {
+        return mae;
+    }
+
+    public void setMae(String mae) {
+        this.mae = mae;
+    }
+
+    public String getPai() {
+        return pai;
+    }
+
+    public void setPai(String pai) {
+        this.pai = pai;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ src/br/com/digitalhouse/Principal.java
@@ -0,0 +1,59 @@
+package br.com.digitalhouse;
+
+public static void main(String[] args) {
+
+public class Principal {
+
+    public static String maeDe(Filho novoFilho) {
+        return novoFilho.getMae();
+    }
+
+    public static String paiDe(Filho novoFilho) {
+        return novoFilho.getPai();
+    }
+
+    public static boolean mesmaMae(Filho filhoUm, Filho filhoDois){
+        return true;
+    }
+
+    public static boolean mesmoPai(Filho filhoUm, Filho filhoDois) {
+        if (paiDe(filhoUm) == (filhoDois)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+}
+
+
+
+/*
+    public static void main(String[] args) {
+
+        //Instanciando objetos
+        Filho robson = new Filho();
+        Filho jaqueline = new Filho();
+        Filho julio = new Filho();
+        Filho michele = new Filho();
+
+        //
+        robson.setMae("Ana");
+        jaqueline.setMae("Ana");
+        julio.setMae("Cristina");
+        michele.setMae("Ana");
+
+        //
+        robson.setPai("José");
+        jaqueline.setPai("José");
+        julio.setPai("José");
+        michele.setPai("José");
+
+        //
+
+
+
+
+    }
+}
